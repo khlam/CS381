4e55e39f5e746e0de6ecbec0fa1f394dfa782cf6
lamki <lamki@oregonstate.edu>
2018-01-31 18:43:01 -0800

    done

--- HW2/MiniLogo.hs
+++ HW2/MiniLogo.hs
@@ -1,10 +1,11 @@
 -- Riley Rimer - rimerr - 932-439-548
 -- River Hendriksen - hendriri - 932239742
 -- Kin-Ho Lam - lamki - 932-435-938
-
+module Minilogo where
+import Data.List
 import Prelude
 
-type Num	=	Int
+-- type Num	=	Int
 type Var	=   String
 type Macro	=	String
  			
@@ -15,8 +16,9 @@ data Mode   =	Down   --- pen status
     deriving(Eq,Show)
      
 data Expr	=	Vari Var	--- Variable reference
-            |	Number Num	--- literal number
+            |	Number Int	--- literal number
             |	Add Expr Expr	--- addition expression
+    deriving(Eq, Show)
  			
 data Cmd	=	Pen Mode ---change pen mode
             |	Move Expr Expr ---move pen to a new position
@@ -57,8 +59,8 @@ nix = Define "nix"
 steps :: Int -> Prog
 steps 0 =	[]
 steps n =	[
-				Call "line" [Vari x, Vari x, Vari (x-1), Vari y],
-				Call "line" [Vari (x-1), Vari y, Vari (x-1), Vari (y-1)]
+				Call "line" [Number n, Number n, Number (n-1), Number n],
+				Call "line" [Number (n-1), Number n, Number (n-1), Number (n-1)]
 			] ++ steps (n-1)
 
 macros :: Prog -> [Macro] -- not sure about this function
@@ -66,3 +68,16 @@ macro [] = []
 macros (x: xs) = case x of
 		Define m _ _ -> m:macros xs
 		otherwise -> macros xs
+
+prettyHelp :: Expr -> String
+prettyHelp (Number n) = show n
+prettyHelp (Vari v) = v
+prettyHelp (Add l r) = prettyHelp l ++ " + " ++ prettyHelp r
+
+pretty :: Prog -> String
+pretty [] = ""
+pretty ((Move l r):xs) = "Move (" ++ prettyHelp l ++ ", " ++ prettyHelp r ++ ");" ++ pretty xs
+pretty (Call n x:xs) = n ++ "Call (" ++ intercalate ", " (map prettyHelp x) ++ ");" ++ pretty xs
+pretty (Define a x p:ps) = "Define " ++ a ++ " (" ++ intercalate ", " x ++ ") {" ++ pretty p ++ "};" ++ pretty ps
+pretty (Pen Up:xs) = "Pen Up;" ++ pretty xs
+pretty (Pen Down:xs) = "Pen Down;" ++ pretty xs
