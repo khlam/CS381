c29aa26ca3df678580be14f9b516b4d01d8e8b10
RivH <hendriri@oregonstate.edu>
2018-01-31 19:13:49 -0800

    Add files via upload
    idk

--- HW2/MiniLogo.hs
+++ HW2/MiniLogo.hs
@@ -5,33 +5,33 @@ module Minilogo where
 import Data.List
 import Prelude
 
--- type Num	=	Int
-type Var	=   String
-type Macro	=	String
- 			
-type Prog	=	[Cmd]   ---sequence of commands
- 			
-data Mode   =	Down   --- pen status
-            |   Up	
-    deriving(Eq,Show)
-     
-data Expr	=	Vari Var	--- Variable reference
-            |	Number Int	--- literal number
-            |	Add Expr Expr	--- addition expression
-    deriving(Eq, Show)
- 			
-data Cmd	=	Pen Mode ---change pen mode
-            |	Move Expr Expr ---move pen to a new position
-            |	Define Macro [Var] Prog ---define a macro
-            |	Call Macro [Expr]	---invoke a macro
+-- type Num =   Int
+type Var = String
+type Macro = String
+    
+type Prog = [Cmd]   ---sequence of commands
+
+data Mode = Down   --- pen status
+            | Up    
     deriving(Eq,Show)
 
---	define line (x1, y1, x2, y2) {
---		pen up;
---		move (x1, y1);
---		pen down;
---		move (x2, y2); 	
---	}
+data Expr   = Vari Var  --- Variable reference
+            | Number Int    --- literal number
+            | Add Expr Expr --- addition expression
+    deriving(Eq, Show)
+    
+data Cmd    = Pen Mode ---change pen mode
+            | Move Expr Expr ---move pen to a new position
+            | Define Macro [Var] Prog ---define a macro
+            | Call Macro [Expr] ---invoke a macro
+    deriving(Eq,Show)
+
+--  define line (x1, y1, x2, y2) {
+--      pen up;
+--      move (x1, y1);
+--      pen down;
+--      move (x2, y2);  
+--  }
 
 line :: Cmd
 line = Define "line"
@@ -43,10 +43,10 @@ line = Define "line"
             Move (Vari "x2") (Vari "y2")
         ]
 
---	define nix (x, y, w, h){
---		call line(x, y, x+w, y+h);
---		call line(x+w, y, x, y+h);
---	}
+--  define nix (x, y, w, h){
+--      call line(x, y, x+w, y+h);
+--      call line(x+w, y, x, y+h);
+--  }
 
 nix :: Cmd
 nix = Define "nix"
@@ -57,17 +57,17 @@ nix = Define "nix"
         ]
 
 steps :: Int -> Prog
-steps 0 =	[]
-steps n =	[
-				Call "line" [Number n, Number n, Number (n-1), Number n],
-				Call "line" [Number (n-1), Number n, Number (n-1), Number (n-1)]
-			] ++ steps (n-1)
+steps 0 =   []
+steps n =   [
+                Call "line" [Number n, Number n, Number (n-1), Number n],
+                Call "line" [Number (n-1), Number n, Number (n-1), Number (n-1)]
+            ] ++ steps (n-1)
 
 macros :: Prog -> [Macro] -- not sure about this function
 macro [] = []
 macros (x: xs) = case x of
-		Define m _ _ -> m:macros xs
-		otherwise -> macros xs
+        Define m _ _ -> m:macros xs
+        otherwise  -> macros xs
 
 prettyHelp :: Expr -> String
 prettyHelp (Number n) = show n
@@ -81,3 +81,20 @@ pretty (Call n x:xs) = n ++ "Call (" ++ intercalate ", " (map prettyHelp x) ++ "
 pretty (Define a x p:ps) = "Define " ++ a ++ " (" ++ intercalate ", " x ++ ") {" ++ pretty p ++ "};" ++ pretty ps
 pretty (Pen Up:xs) = "Pen Up;" ++ pretty xs
 pretty (Pen Down:xs) = "Pen Down;" ++ pretty xs
+
+--Define a Haskell function optE :: Expr -> Expr that partially evaluates expressions by replacing any additions of literals with the result. 
+--For example, given the expression (2+3)+x, optE should return the expression 5+x.
+optE :: Expr -> Expr
+optE (Add(Number l)(Number r)) = Number (l+r)
+optE otherwise = otherwise
+
+--Define a Haskell function optP :: Prog -> Prog that optimizes all of the expressions contained in a given program using optE.
+optP :: Prog -> Prog
+optP [] = []
+optP (x: xs) = case x of
+        Move l r -> Move(optE l) (optE r):optP xs
+        Define m va p -> Define m va (optP p):optP xs
+        Call m exer -> Call m (map optE exer): optP xs
+        otherwise -> x:optP xs 
+
+
